0aa1139a9fa47547ed40eca88834db13fedc76c5
Tsubasa Umeuchi <tsubasa.umeuchi@gmail.com>
2023-12-17 19:44:15 +0900

    Update analysis.sh script to include additional filtering options for alp

--- analysis.sh
+++ analysis.sh
@@ -2,5 +2,5 @@
 
 now=$(date +"%Y%m%d_%H%M%S")
 
-sudo cat /var/log/nginx/access.log | alp > ./alp/alp${now}.log
+sudo cat /var/log/nginx/access.log | alp ltsv -m "/api/livestream/\d+/report,/api/livestream/\d+/statistics,/api/user/.+/statistics,/api/livestream/\d+/moderate,api/livestream/\d+/reaction,/api/livestream/\d+/livecomment,/api/user/.+/icon,/api/livestream/\d+/enter,/api/livestream/\d+/exit,/api/livestream/\d+/ngword,/api/user/.+/theme" -o count,uri,min,max,avg,sum --sort sum -r > ./alp/alp${now}.log
 sudo mysqldumpslow /var/log/mysql/mysql-slow.log > ./mysqldumpslow/mysqldumpslow_${now}.log
