dd180ab4bc6b708ea7f17baff27a4a2548d1f9cc
Tsubasa Umeuchi <tsubasa.umeuchi@gmail.com>
2023-12-17 19:30:49 +0900

    Update alp command in analysis.sh

--- analysis.sh
+++ analysis.sh
@@ -2,5 +2,5 @@
 
 now=$(date +"%Y%m%d_%H%M%S")
 
-sudo cat /var/log/nginx/access.log | alp ltsv --sort=avg --reverse > ./alp/alp${now}.log
+sudo cat /var/log/nginx/access.log | alp --sort=avg --reverse > ./alp/alp${now}.log
 sudo mysqldumpslow /var/log/mysql/mysql-slow.log > ./mysqldumpslow/mysqldumpslow_${now}.log
