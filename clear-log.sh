8363a647c6c7a8f19d32261d8f2287c09a8d3e70
Tsubasa Umeuchi <tsubasa.umeuchi@gmail.com>
2023-12-18 00:46:48 +0900

    Update clear-log.sh script with sudo permissions

--- clear-log.sh
+++ clear-log.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-cp /dev/null /var/log/nginx/access.log
-cp /dev/null /var/log/mysql/mysql-slow.log
+sudo cp /dev/null /var/log/nginx/access.log
+sudo cp /dev/null /var/log/mysql/mysql-slow.log
 
 if [ ! -d "/tmp/icons" ]; then
-    mkdir /tmp/icons
+    sudo mkdir /tmp/icons
 fi
-rm -rf /tmp/icons/*
+sudo rm -rf /tmp/icons/*
 
 cd webapp/go
 make build
 sudo systemctl restart isupipe-go
+cd ../../
