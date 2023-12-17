9483150415877eb23d45e91747a710298184a027
Tsubasa Umeuchi <tsubasa.umeuchi@gmail.com>
2023-12-17 20:23:56 +0900

    Refactor image retrieval in user_handler.go

--- webapp/go/user_handler.go
+++ webapp/go/user_handler.go
@@ -390,15 +390,12 @@ func fillUserResponse(ctx context.Context, tx *sqlx.Tx, userModel UserModel) (Us
 	}
 
 	var image []byte
-	if err := tx.GetContext(ctx, &image, "SELECT image FROM icons WHERE user_id = ?", userModel.ID); err != nil {
-		if !errors.Is(err, sql.ErrNoRows) {
-			return User{}, err
-		}
-		image, err = os.ReadFile(fallbackImage)
-		if err != nil {
-			return User{}, err
-		}
+	username := userModel.Name
+	image, err := os.ReadFile(fmt.Sprintf("/tmp/icons/%s.jpg", username))
+	if err != nil {
+		image = []byte(fallbackImage)
 	}
+
 	iconHash := sha256.Sum256(image)
 
 	user := User{
